e74e79a79046cb61719e15226c78fe36027c87da
Bella Lo <bellalo@BestaMacBook-Air.local>
2018-05-27 21:53:09 +0800

    adding database connection

--- server.js
+++ server.js
@@ -14,10 +14,9 @@ const image = require('./controllers/image');
 const db = knex({
   client: 'pg',
   connection: {
-    host : 'postgresql-silhouetted-78726',
-    user : 'bellalo',
-    password : '',
-    database : 'smart-brain-database'
+    host : process.env.DATABASE_URL,
+    ssl: true
+
   }
 });
 
